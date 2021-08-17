5ca6c7aa84316fc4e296f2546493e921e97cd275
Uzumaki Naruto <nguyenduyhanlam@yahoo.com.vn>
2021-08-17 10:46:37 +0700

    update

--- fairseq/scripts/run.sh
+++ fairseq/scripts/run.sh
@@ -8,7 +8,7 @@ train_model(){
     modeldir=$workdir/models/${src}2${tgt}_vanilla && mkdir -p $modeldir
     
     echo "train vanilla transformer for ${src} to ${tgt}..."
-    python train.py $workdir/processed_data \
+    python $codedir/train.py $workdir/processed_data \
         -a transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s $src -t $tgt \
         --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  --seed 32  \
         --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 --activation-fn relu \
@@ -31,7 +31,7 @@ train_AET_transformer(){
     cp $workdir/models/${tgt}2${src}_vanilla/checkpoint_best.pt $modeldir/checkpoint_last_b.pt 
     
     echo "train alignment enhanced transformer (AET) for ${src} to ${tgt}..."
-    python train.py $workdir/processed_data \
+    python $codedir/train.py $workdir/processed_data \
         -a dual_transformer_small --optimizer adam --lr 0.0005 -s $src -t $tgt \
         --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  --seed 32  \
         --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 --activation-fn relu \
@@ -53,18 +53,18 @@ extract_bidirectional_alignment(){
     for split in 'train' 'valid'; do
         echo "start to extract ${src}2${tgt} alignment."
         Model=$workdir/models/${src}2${tgt}_vanilla
-        python generate_align.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
+        python $codedir/generate_align.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
             --max-tokens 8000 --beam 1 --remove-bpe --quiet --print-vanilla-alignment --decoding-path $resdir  \
             --alignment-task vanilla --gen-subset $split --alignment-layer 2 --set-shift 
             
         echo "start to extract ${tgt}2${src} alignment."
         Model=$workdir/models/${tgt}2${src}_vanilla
-        python generate_align.py $workdir/processed_data -s $tgt -t $src --path $Model/checkpoint_best.pt \
+        python $codedir/generate_align.py $workdir/processed_data -s $tgt -t $src --path $Model/checkpoint_best.pt \
             --max-tokens 8000 --beam 1 --remove-bpe --quiet --print-vanilla-alignment --decoding-path $resdir  \
             --alignment-task vanilla --gen-subset $split --alignment-layer 2 --set-shift 
 
         method="grow-diagonal"
-        python scripts/aer/combine_bidirectional_alignments.py $resdir/$split.${src}2${tgt}.align $resdir/$split.${tgt}2${src}.align --method $method > $resdir/$split.${src}2${tgt}.bidir.align
+        python $codedir/scripts/aer/combine_bidirectional_alignments.py $resdir/$split.${src}2${tgt}.align $resdir/$split.${tgt}2${src}.align --method $method > $resdir/$split.${src}2${tgt}.bidir.align
     done 
     echo "successfully extract $src-$tgt bidirectional alignment. Now you need to re-processe your data with generated alignment."
 }
@@ -81,25 +81,25 @@ calculate_AET_att_AER(){
     split='test'
     Model=$workdir/models/${src}2${tgt}_aet
     echo "start to extract alignment with AET method."
-    python extract_dual_alignment.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
+    python $codedir/extract_dual_alignment.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
         --max-tokens 4000 --beam 1 --remove-bpe --left-pad-source True --left-pad-target False \
         --decoding-path $resdir --quiet --print-vanilla-alignment --alignment-task 'supalign' \
         --gen-subset $split --alignment-layer 2 --set-shift --set-dual-trans 
     
-    python scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
+    python $codedir/scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
        --tgt $bpe/test.$tgt --input $resdir/$split.${src}2${tgt}.bidual.align --output $resdir/$split.${src}2${tgt}.bidual.raw.align
-    python scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
+    python $codedir/scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
        --tgt $bpe/test.$tgt --input $resdir/$split.${src}2${tgt}.dualf.align --output $resdir/$split.${src}2${tgt}.dualf.raw.align 
-    python scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$tgt \
+    python $codedir/scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$tgt \
        --tgt $bpe/test.$src --input $resdir/$split.${src}2${tgt}.dualb.align --output $resdir/$split.${src}2${tgt}.dualb.raw.align
 
     method="grow-diagonal"
     python scripts/aer/combine_bidirectional_alignments.py $resdir/$split.${src}2${tgt}.dualf.raw.align $resdir/$split.${src}2${tgt}.dualb.raw.align --method $method > $resdir/$split.${src}2${tgt}.bidir.aet.align
 
     echo "=====AER is start to calculate AER for align then merge. ..."   
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.dualf.raw.align --fAlpha 0.5 --oneRef 
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.dualb.raw.align --fAlpha 0.5 --oneRef --reverseHyp
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.bidir.aet.align --fAlpha 0.5 --oneRef 
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.dualf.raw.align --fAlpha 0.5 --oneRef 
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.dualb.raw.align --fAlpha 0.5 --oneRef --reverseHyp
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.bidir.aet.align --fAlpha 0.5 --oneRef 
 }
 
 calculate_shift_att_AER(){   
@@ -113,30 +113,30 @@ calculate_shift_att_AER(){
     split='test'
     echo "start to extract ${src}2${tgt} alignment."
     Model=$workdir/models/${src}2${tgt}_vanilla
-    python generate_align.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
+    python $codedir/generate_align.py $workdir/processed_data -s $src -t $tgt --path $Model/checkpoint_best.pt \
         --max-tokens 4000 --beam 1 --remove-bpe --quiet --print-vanilla-alignment --decoding-path $resdir  \
         --alignment-task vanilla --gen-subset $split --alignment-layer 2 --set-shift 
     
-    python scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
+    python $codedir/scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$src \
        --tgt $bpe/test.$tgt --input $resdir/$split.${src}2${tgt}.align --output $resdir/$split.${src}2${tgt}.raw.align
 
     echo "start to extract ${tgt}2${src} alignment." 
     Model=$workdir/models/${tgt}2${src}_vanilla
-    python generate_align.py $workdir/processed_data -s $tgt -t $src --path $Model/checkpoint_best.pt \
+    python $codedir/generate_align.py $workdir/processed_data -s $tgt -t $src --path $Model/checkpoint_best.pt \
         --max-tokens 4000 --beam 1 --remove-bpe --quiet --print-vanilla-alignment --decoding-path $resdir  \
         --alignment-task vanilla --gen-subset $split --alignment-layer 2 --set-shift 
     
-    python scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$tgt \
+    python $codedir/scripts/aer/sentencepiece_to_word_alignments.py --src $bpe/test.$tgt \
        --tgt $bpe/test.$src --input $resdir/$split.${tgt}2${src}.align --output $resdir/$split.${tgt}2${src}.raw.align
 
     echo "start to merge alignments ..."
     method="grow-diagonal"
-    python scripts/aer/combine_bidirectional_alignments.py $resdir/$split.${src}2${tgt}.raw.align  $resdir/$split.${tgt}2${src}.raw.align --method $method > $resdir/$split.${src}2${tgt}.bidir.align
+    python $codedir/scripts/aer/combine_bidirectional_alignments.py $resdir/$split.${src}2${tgt}.raw.align  $resdir/$split.${tgt}2${src}.raw.align --method $method > $resdir/$split.${src}2${tgt}.bidir.align
 
     echo "=====calculate AER for shift-att method=========="   
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.raw.align --fAlpha 0.5 --oneRef 
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${tgt}2${src}.raw.align --fAlpha 0.5 --oneRef --reverseHyp
-    python scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.bidir.align --fAlpha 0.5 --oneRef 
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.raw.align --fAlpha 0.5 --oneRef 
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${tgt}2${src}.raw.align --fAlpha 0.5 --oneRef --reverseHyp
+    python $codedir/scripts/aer/aer.py ${ref_align} $resdir/$split.${src}2${tgt}.bidir.align --fAlpha 0.5 --oneRef 
 }
 
 set -e 
@@ -146,6 +146,7 @@ export CUDA_VISIBLE_DEVICES=1
 src=vi 
 tgt=en 
 workdir=/content/drive/MyDrive/ChuyenDeNghienCuuKhoaHoc
+codedir=/content/ChuyenDeNghienCuuKhoaHoc/fairseq
 # follow https://github.com/lilt/alignment-scripts to process training data
 # processed data is in $workdir/processed_data dir.
 
