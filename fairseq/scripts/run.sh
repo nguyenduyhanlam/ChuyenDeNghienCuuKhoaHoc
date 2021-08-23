2aa343f199abe224b27a5729c56c80a6435567ce
Uzumaki Naruto <nguyenduyhanlam@yahoo.com.vn>
2021-08-23 10:11:34 +0700

    update run.sh v0.9.0

--- fairseq/scripts/run.sh
+++ fairseq/scripts/run.sh
@@ -9,8 +9,8 @@ train_model(){
     
     echo "train vanilla transformer for ${src} to ${tgt}..."
     python $codedir/train.py $workdir/processed_data \
-        -a transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s $src -t $tgt \
-        --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  --seed 32  \
+        -a transformer_wmt_en_de_big --optimizer adam --lr 0.0005 -s $src -t $tgt \
+        --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  \
         --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0 --activation-fn relu \
         --criterion label_smoothed_cross_entropy --max-update $MaxUpdates --clip-norm 0.0 \
         --warmup-updates 4000 --warmup-init-lr '1e-07' --keep-last-epochs 200 \
@@ -33,7 +33,7 @@ train_AET_transformer(){
     echo "train alignment enhanced transformer (AET) for ${src} to ${tgt}..."
     python $codedir/train.py $workdir/processed_data \
         -a dual_transformer_small --optimizer adam --lr 0.0005 -s $src -t $tgt \
-        --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  --seed 32  \
+        --label-smoothing 0.1 --dropout 0.3 --max-tokens 4500  \
         --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0 --activation-fn relu \
         --criterion label_smoothed_cross_entropy_dual_trans --max-update $MaxUpdates --clip-norm 0.0 \
         --warmup-updates 4000 --warmup-init-lr '1e-07' --keep-last-epochs 100  \
